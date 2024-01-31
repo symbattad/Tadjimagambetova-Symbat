37c824010a21a490546add6853fb339fe2baad56
symbattad <s_tajimagambetova@kbtu.kz>
2024-01-31 20:36:15 +0600

    new file

---
+++ Lab2/lab2cont/addsetItems.py
@@ -0,0 +1,27 @@
+#EXAMPLE
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
+
+
+
+
